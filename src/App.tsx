c5954f781ed553e16b644c4af4cda4dab624418a
Rudolph Christian Razul <razulchristian@gmail.com>
2024-03-16 19:01:45 +0800

    Update React Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import ModeSelector from './components/ModeSelector';
 import LandingPage from './pages/LandingPage';
 import ScoreboardPage from './pages/ScoreboardPage';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <header>
-          <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/scoreboard'>Scoreboard</NavLink>
-          </nav>
-        </header>
-        <Routes>
-          <Route index element={<LandingPage />} />
-          <Route path='/scoreboard' element={<ScoreboardPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path='scoreboard' element={<ScoreboardPage />} />
+      </Routes>
     </>
   );
 }
--- src/components/ModeSelector.tsx
+++ src/components/ModeSelector.tsx
@@ -1,15 +1,20 @@
-import { Delete, EmojiEvents, EmojiEventsOutlined, Group, ScoreboardOutlined } from '@mui/icons-material';
+import { EmojiEventsOutlined, ScoreboardOutlined } from '@mui/icons-material';
 import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 const ModeSelector = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '24px', padding: '2rem' }}>
-      <Button variant='contained' size='large' endIcon={<EmojiEventsOutlined />}>
-        Tournament Mode
-      </Button>
-      <Button variant='contained' size='large' endIcon={<ScoreboardOutlined />}>
-        Scoreboard Mode
-      </Button>
+      <NavLink to='/tournament'>
+        <Button variant='contained' size='large' endIcon={<EmojiEventsOutlined />}>
+          Tournament Mode
+        </Button>
+      </NavLink>
+      <NavLink to='/scoreboard'>
+        <Button variant='contained' size='large' endIcon={<ScoreboardOutlined />}>
+          Scoreboard Mode
+        </Button>
+      </NavLink>
     </div>
   );
 };
