9fd6d7e8d3c489c5d9628465b84edce255210351
Rudolph Christian Razul <razulchristian@gmail.com>
2024-03-26 22:53:41 +0800

    Build the TournamentForm

--- src/components/TournamentForm.tsx
+++ src/components/TournamentForm.tsx
@@ -1,12 +1,99 @@
-import { TextField } from '@mui/material';
+import { TextField, Button, Stack, InputLabel, Select, MenuItem, SelectChangeEvent, FormControl } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 import '../App.css';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
+type FormValues = {
+  playerCount: number;
+  maxScore: number;
+  tournamentType: string;
+};
 
 const TournamentForm = () => {
+  const [tournamentType, setTournamentType] = useState('');
+
+  const form = useForm<FormValues>({
+    defaultValues: {
+      playerCount: 2,
+      maxScore: 1,
+      tournamentType: 'tm-single-elim',
+    },
+  });
+
+  const { register, handleSubmit, formState, control } = form;
+
+  const { errors } = formState;
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTournamentType(event.target.value as string);
+  };
+
   return (
-    <form noValidate autoComplete='off'>
-      <TextField id='standard-basic' label='Player Count' variant='outlined' fullWidth required />
-      <div className='test2'></div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete='off'>
+        <Stack spacing={2} width={400}>
+          <TextField
+            id='standard-basic'
+            label='Player Count'
+            variant='outlined'
+            type='number'
+            fullWidth
+            required
+            {...register('playerCount', {
+              required: 'Player count is required',
+            })}
+            error={!!errors.playerCount}
+            helperText={errors.playerCount?.message}
+          />
+          <TextField
+            id='standard-basic'
+            label='Goal Score'
+            variant='outlined'
+            fullWidth
+            required
+            {...register('maxScore', {
+              required: 'Max Score is required',
+            })}
+            error={!!errors.maxScore}
+            helperText={errors.maxScore?.message}
+          />
+          <FormControl fullWidth>
+            <InputLabel id='demo-simple-select-label'>Tournament Mode</InputLabel>
+            <Select
+              id='tournament-type-select'
+              value={tournamentType}
+              label='Tournament Type'
+              {...register('tournamentType', {
+                required: 'Tournament Type is required',
+              })}
+              onChange={handleChange}
+              style={{ textAlign: 'left' }}
+            >
+              <MenuItem value={'tm-single-elim'}>Single Elimination</MenuItem>
+              <MenuItem value={'tm-double-elim'}>Double Elimination</MenuItem>
+            </Select>
+          </FormControl>
+          <Stack direction={'row'} spacing={2}>
+            <NavLink to='/scoreboard' style={{ width: '100%' }}>
+              <Button type='button' variant='outlined' fullWidth>
+                Back
+              </Button>
+            </NavLink>
+
+            <Button type='submit' variant='contained' fullWidth>
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 
--- src/pages/TournamentPage.tsx
+++ src/pages/TournamentPage.tsx
@@ -3,6 +3,8 @@ import TournamentBracket from '../components/TournamentBracket';
 import { players } from '../common/players';
 import { useState } from 'react';
 import Scoreboard, { Match, initialMatchState } from '../components/Scoreboard/Scoreboard';
+import TournamentForm from '../components/TournamentForm';
+import BasicSelect from '../components/testTournamentForm';
 
 const TournamentPage = () => {
   const [matchDetails, setMatchDetails] = useState<Match>(initialMatchState);
@@ -12,8 +14,10 @@ const TournamentPage = () => {
   return (
     <Box>
       {/* <TournamentBracket players={players} match={matchDetails} winner={matchDetails.winner || initialPlayerState} /> */}
-      <TournamentBracket match={matchDetails} />
-      <Scoreboard players={players} maxScore={4} match={match => setMatchDetails(match)} />
+      {/* <TournamentBracket match={matchDetails} />
+      <Scoreboard players={players} maxScore={4} match={match => setMatchDetails(match)} /> */}
+      <TournamentForm />
+      <BasicSelect />
     </Box>
   );
 };
